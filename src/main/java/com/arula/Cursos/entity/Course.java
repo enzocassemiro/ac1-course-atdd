d303b9f1e7a8bb019ffb7c219be77f6d1e4d53ac
AlexanderPoliser <alexanderpoliser@gmail.com>
2023-09-23 01:27:26 -0300

    refactor(Course and Student entities): refactoring Course and Student entities to model database

--- src/main/java/com/arula/Cursos/entity/Course.java
+++ src/main/java/com/arula/Cursos/entity/Course.java
@@ -1,18 +1,43 @@
 package com.arula.Cursos.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity(name = "course")
 public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
     private String name;
-    private Integer hours;
+
+    @Column(nullable = false)
+    private Double hours;
+
+    @Column(nullable = false)
     private Integer availableSpots;
+
+    @ManyToMany
+    @JoinTable(
+            name = "course_student",
+            joinColumns = @JoinColumn(name = "course_id"),
+            inverseJoinColumns = @JoinColumn(name = "student_id")
+    )
+    @Cascade({CascadeType.ALL})
+    private List<Student> students;
+
 
     public boolean validateStudentOnCourse(Student student) {
         for (Course course : student.getCourses()) {
